dbff074fdda31f0c4ef22b1c571829aec0dd9e4c
Sergei Tumanov <sergeitumanov.web@gmail.com>
2026-03-25 16:50:39 +0300

    Update index.php
    Task#2

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<?php 
-    header('Content-type: text/plain; charset=UTF-8');
-    header ('Access-Control-Allow-Origin: *');
-    header ('x-author: sergei.t.web');
-?> function task () { return x * this * this; }
+<?php
+    header('Content-Type: text/plain; charset=UTF-8');
+    header('Access-Control-Allow-Origin: *');
+
+    $requestUri = $_SERVER['REQUEST_URI'];
+    $path = parse_url($requestUri, PHP_URL_PATH);
+
+
+    if ($path === '/login/') {
+        echo 'sergei.t.web';
+    } elseif ($path === '/sample/') {
+        echo 'function task(x) { return x * (this ** 2); }';
+    } else {
+        http_response_code(404);
+        echo 'Not Found';
+    }
+?>
