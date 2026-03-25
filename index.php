8b038f6f8dadfd7bd5fde75938b07d2593f19647
Sergei Tumanov <sergeitumanov.web@gmail.com>
2026-03-25 16:55:42 +0300

    Update index.php
    empty .php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php
-    header('Content-Type: text/plain; charset=UTF-8');
-    header('Access-Control-Allow-Origin: *');
 
-    $requestUri = $_SERVER['REQUEST_URI'];
-    $path = parse_url($requestUri, PHP_URL_PATH);
-
-
-    if ($path === '/login/') {
-        echo 'sergei.t.web';
-    } elseif ($path === '/sample/') {
-        echo 'function task(x) { return x * (this ** 2); }';
-    } else {
-        http_response_code(404);
-        echo 'Not Found';
-    }
 ?>
