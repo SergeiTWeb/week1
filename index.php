333ba02458ea5821ffcb1e2c660a4c3002e06e22
Sergei Tumanov <sergeitumanov.web@gmail.com>
2026-03-25 16:45:28 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+    header('Content-type: text/plain; charset=UTF-8');
+    header ('Access-Control-Allow-Origin: *');
+    header ('x-author: sergei.t.web');
+?> function task () { return x * this * this; }
